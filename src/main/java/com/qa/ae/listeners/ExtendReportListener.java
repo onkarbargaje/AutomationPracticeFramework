c6f28140160d94feca99eb1927dcaf308d623b31
onkarbargaje <bargajeonkar99@gmail.com>
2024-07-29 19:08:56 +0530

    commit another repo

--- src/main/java/com/qa/ae/listeners/ExtendReportListener.java
+++ src/main/java/com/qa/ae/listeners/ExtendReportListener.java
@@ -97,7 +97,7 @@ public class ExtendReportListener implements ITestListener  {
 		String methodName = result.getMethod().getMethodName();
 		System.out.println((methodName + " passed!"));
 		test.get().pass("Test passed");
-		test.get().pass(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(DriverFactory.getScreenshot(methodName), methodName).build());
+		//test.get().pass(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(DriverFactory.getScreenshot(methodName), methodName).build());
 		test.get().getModel().setEndTime(getTime(result.getEndMillis()));
 	}
 
